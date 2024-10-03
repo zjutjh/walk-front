ac5941cd73f79e29d95dcda55695407a1a50c65c
popWheat <49830650+j10ccc@users.noreply.github.com>
2024-10-03 23:15:49 +0800

    feat(dev): 开发环境添加虚拟控制台 (#3)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import 'vfonts/Lato.css'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import VConsole from 'vconsole'
 import { routes } from './routes'
 
 // 设置前端路由
@@ -14,3 +15,10 @@ const router = createRouter({
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+
+if (import.meta.env.DEV) {
+  setTimeout(() => {
+    new VConsole();
+    console.warn("DEV 环境下 VConsole 启动")
+  })
+}
